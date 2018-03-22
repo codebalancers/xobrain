77d4912613af2a13ba346837e4a1de46e9832255
Arne Bosien <info@arnebosien.de>
2018-03-22 22:25:19 +0100

    electron setup

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,61 @@
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, Menu, MenuItemConstructorOptions } from 'electron'
+import { join as pJoin } from 'path'
+import { format as urlFormat } from 'url'
 
-let win = null;
+let mainWindow = null;
 
 app.on('ready', () => {
-    win = new BrowserWindow({width: 800, height: 600})
+    mainWindow = new BrowserWindow({});
+
+    mainWindow.loadURL(urlFormat({
+        pathname: pJoin(__dirname, '../index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // close whole app when main window is closed
+    mainWindow.on('close', () => app.quit());
+
+    // set up menu
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
 });
+
+const mainMenuTemplate: MenuItemConstructorOptions[] = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Quit',
+                accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+// in mac push empty menu to beginning
+if (process.platform === 'darwin') {
+    mainMenuTemplate.unshift({});
+}
+
+// Add developer tools option if in dev
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [
+            {
+                role: 'reload'
+            },
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform === 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow) {
+                    focusedWindow.webContents.toggleDevTools();
+                }
+            }
+        ]
+    });
+}
