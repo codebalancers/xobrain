bbe37f16ec91179bdc9f6ce8cfe178efc4e03c9e
Arne Bosien <info@arnebosien.de>
2018-03-25 16:18:17 +0200

    fix refresh of md editor

--- src/presentation/visuals/mde/md-editor.component.ts
+++ src/presentation/visuals/mde/md-editor.component.ts
@@ -1,14 +1,28 @@
 import { AfterViewInit, Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import * as SimpleMDE from 'simplemde';
+import { LangUtils } from '../../../util/lang.utils';
+import { StringUtils } from '../../../util/string.utils';
 
 @Component({
   selector: 'md-editor',
   template: '<textarea  #simplemde></textarea>',
 })
 export class MdEditorComponent implements AfterViewInit {
-  @Input() viewMode = false;
+  private _model: string;
 
-  @Input() model: string;
+  @Input() set model(val: string) {
+    this._model = val;
+
+    if (LangUtils.isDefined(this.editor)) {
+      if (StringUtils.isBlank(val)) {
+        // setting undefined does not work, we need to provide an empty string
+        this.editor.value('');
+      } else {
+        this.editor.value(val);
+      }
+    }
+  }
+
   @Output() modelChange = new EventEmitter<string>();
 
   @ViewChild('simplemde') textarea: ElementRef;
@@ -16,28 +30,19 @@ export class MdEditorComponent implements AfterViewInit {
   private editor: SimpleMDE;
 
   ngAfterViewInit() {
-    if (this.viewMode) {
-      this.editor = new SimpleMDE({
-        element: this.textarea.nativeElement,
-        toolbar: false,
-        status: false
-      });
+    this.editor = new SimpleMDE({
+      element: this.textarea.nativeElement,
+      spellChecker: false,
+      // forceSync: true,
+      showIcons: ['code'],
+      hideIcons: ['preview', 'side-by-side', 'fullscreen', 'guide', 'image', 'table']
+    });
 
-      (this.editor as any).togglePreview();
-    } else {
-      this.editor = new SimpleMDE({
-        element: this.textarea.nativeElement,
-        spellChecker: false,
-        // forceSync: true,
-        showIcons: [ 'code' ],
-        hideIcons: [ 'preview', 'side-by-side', 'fullscreen', 'guide', 'image', 'table' ]
-      });
+    this.editor.codemirror.on('change', () => {
+      this._model = this.editor.value();
+      this.modelChange.emit(this.editor.value());
+    });
 
-      this.editor.codemirror.on('change', () => {
-        this.modelChange.emit(this.editor.value())
-      });
-    }
-
-    this.editor.value(this.model);
+    this.editor.value(this._model);
   }
 }
