69afb6aa6678bcb976776be7aa88ed91a95bf4e3
Arne Bosien <info@arnebosien.de>
2018-03-27 10:15:26 +0200

    add NOT NULL to db

--- src/business/control/database.service.ts
+++ src/business/control/database.service.ts
@@ -6,7 +6,7 @@ import { StringUtils } from '../../util/string.utils';
 const init_sql_1 = `
   CREATE TABLE IF NOT EXISTS card(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    modificationDate DATE,
+    modificationDate DATE NOT NULL,
     title CHAR(180),
     content TEXT
   );
@@ -15,7 +15,7 @@ const init_sql_1 = `
 const init_sql_2 = `
   CREATE TABLE IF NOT EXISTS file(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    modificationDate DATE,
+    modificationDate DATE NOT NULL,
     data BLOB
   );
   `;
@@ -23,16 +23,16 @@ const init_sql_2 = `
 const init_sql_3 = `
   CREATE TABLE IF NOT EXISTS tag(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    modificationDate DATE,
+    modificationDate DATE NOT NULL,
     name CHAR(100)
   );
   `;
 
 const init_sql_4 = `  
   CREATE TABLE IF NOT EXISTS card_tag(
-    card_id INTEGER,
-    tag_id INTEGER,
-    modificationDate DATE,
+    card_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
+    modificationDate DATE NOT NULL,
     FOREIGN KEY(card_id) REFERENCES card,
     FOREIGN KEY(tag_id) REFERENCES tag,
     UNIQUE (card_id, tag_id) ON CONFLICT IGNORE
@@ -41,10 +41,10 @@ const init_sql_4 = `
 
 const init_sql_5 = `  
   CREATE TABLE IF NOT EXISTS card_card(
-    card1_id INTEGER,
-    card2_id INTEGER,
-    weight REAL,
-    modificationDate DATE,
+    card1_id INTEGER NOT NULL,
+    card2_id INTEGER NOT NULL,
+    weight REAL NOT NULL,
+    modificationDate DATE NOT NULL,
     FOREIGN KEY(card1_id) REFERENCES card,
     FOREIGN KEY(card2_id) REFERENCES card,
     UNIQUE (card1_id, card2_id) ON CONFLICT IGNORE
@@ -53,9 +53,9 @@ const init_sql_5 = `
 
 const init_sql_6 = `  
   CREATE TABLE IF NOT EXISTS card_file(
-    card_id INTEGER,
-    file_id INTEGER,
-    modificationDate DATE,
+    card_id INTEGER NOT NULL,
+    file_id INTEGER NOT NULL,
+    modificationDate DATE NOT NULL,
     FOREIGN KEY(card_id) REFERENCES card,
     FOREIGN KEY(file_id) REFERENCES file,
     UNIQUE (card_id, file_id) ON CONFLICT IGNORE
